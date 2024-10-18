ddd8b4ce2b99573b0986d194b5fc640c5260f114
Pedro Place <pedro.place@hotmail.com>
2024-10-18 16:04:18 +0100

    Create ProgramaPernas.java

---
+++ ProgramaPernas.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class ProgramaPernas {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Quantas pernas? ");
+        int pernas = teclado.nextInt();
+        String tipo;
+        System.out.print("Isso é um(a) ");
+        switch (pernas){
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bípede";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6,8:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";
+        }
+        System.out.println(tipo);
+    }
+}
