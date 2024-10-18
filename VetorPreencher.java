a12af195712463c3951544e82f4993307a9c6f27
Pedro Place <pedro.place@hotmail.com>
2024-10-18 20:52:49 +0100

    Create VetorPreencher.java

---
+++ VetorPreencher.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class VetorPreencher {
+    public static void main(String[] args) {
+        int v[] = new int[20];
+        Arrays.fill(v, 0);
+        for (int valor: v){
+            System.out.print(valor + " ");
+        }
+    }
+}
