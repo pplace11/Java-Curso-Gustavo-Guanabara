a3df171b88a88dd60324123ff7d0517b7f94ab3d
Pedro Place <pedro.place@hotmail.com>
2024-10-18 20:52:39 +0100

    Create Vetor03.java

---
+++ Vetor03.java
@@ -0,0 +1,8 @@
+public class Vetor03 {
+    public static void main(String[] args) {
+        double v[] = {3.5, 2.75, 9, -4.5};
+        for (double valor: v){
+            System.out.println(valor + " ");
+        }
+    }
+}
