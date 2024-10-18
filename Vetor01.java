4bf9b3de42a7a14d946498acf66a593a83a7f479
Pedro Place <pedro.place@hotmail.com>
2024-10-18 20:52:25 +0100

    Create Vetor01.java

---
+++ Vetor01.java
@@ -0,0 +1,8 @@
+public class Vetor01 {
+    public static void main(String[] args) {
+        int n [] = {3, 2, 8, 7, 5, 4};
+        for (int c=0; c<=n.length-1; c++){
+            System.out.println("Na posiçao " +  c  + " temos valores " + n[c] + ".");
+        }
+    }
+}
