0fd4aeb477cb0451267ddfeabd08f69c485a4552
Pedro Place <pedro.place@hotmail.com>
2024-10-18 20:52:15 +0100

    Create RepeticaoFor.java

---
+++ RepeticaoFor.java
@@ -0,0 +1,7 @@
+public class RepeticaoFor {
+    public static void main(String[] args) {
+        for (int cc=0; cc<4; cc++){
+            System.out.println("Cambalhota" + cc);
+        }
+    }
+}
