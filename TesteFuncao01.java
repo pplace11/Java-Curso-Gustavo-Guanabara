6ec435a41eeb55c8c03d4ce52ec279bc96da7bba
Pedro Place <pedro.place@hotmail.com>
2024-10-25 14:11:53 +0100

    Create TesteFuncao01.java

---
+++ TesteFuncao01.java
@@ -0,0 +1,9 @@
+public class TesteFuncao01 {
+    public static void main(String[] args) {
+        soma(5, 2);
+    }
+    public static void soma (int a, int b){
+        int s = a + b;
+        System.out.println("A soma de a mais b é " + s);
+    }
+}
