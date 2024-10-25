35c561669d7a297ae28f2415dc0c68b7ce6dd558
Pedro Place <pedro.place@hotmail.com>
2024-10-25 14:11:57 +0100

    Create TesteFucao02.java

---
+++ TesteFucao02.java
@@ -0,0 +1,10 @@
+public class TesteFucao02 {
+    public static void main(String[] args) {
+        int sm = soma(5, 2);
+        System.out.println("A soma vale " + sm);
+    }
+    public static int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+}
